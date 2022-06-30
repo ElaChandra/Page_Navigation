c495bbc1e10facd1713526bf0004bb8512d220ae
ElaChandra <80231948+ElaChandra@users.noreply.github.com>
2022-07-01 01:27:25 +0800

    Add files via upload

---
+++ settings.page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext build) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Pengaturan"),
+        ElevatedButton(onPressed: () {}, child: Text("Ubah Nama"))
+      ],
+    );
+  }
+}
